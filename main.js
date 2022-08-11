b864606bad14f90196f37ffc1493c6dbfd849321
mohit5mishra <93033934+mohit5mishra@users.noreply.github.com>
2022-08-11 16:30:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+noseX=0;
+noseY=0;
+difference=0;
+rightWristX=0;
+leftWristX=0;
+
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas=createCanvas(550,550);
+    canvas.position(560,120);
+}
+
+function draw(){
+    background('#5ecbdb');
+}
+
